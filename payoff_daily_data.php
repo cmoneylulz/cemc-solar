af3f3f31cfb4ae1700070bdf291736336842a5a1
cmoneylulz <element4emerica@gmail.com>
2014-07-17 16:25:48 -0400

    daily payoff data now uses the same averaging method as kwh payoff data

--- payoff_daily_data.php
+++ payoff_daily_data.php
@@ -15,7 +15,8 @@ if (!$connection)
 
 if (!isset($_POST['start_date'])){
     $start_date = date('Y-m-d', strtotime('today - 30 days'));
-} else {
+} 
+else {
     $start_date = $_POST['start_date'];
 }
 
@@ -23,14 +24,16 @@ if (!isset($_POST['end_date'])){
     $date = getdate();
     $current_year = $date['year'];
     $end_date = $date['year'] . "-" . $date['mon'] . "-" . $date['mday'];
-} else {
+}
+else {
     $end_date = $_POST['end_date'];
 }
 
 $query = "SELECT * FROM DCSI.METERACCTSTC WHERE METERTCREADDT between to_date('$start_date', 'yyyy-mm-dd') and to_date('$end_date', 'yyyy-mm-dd') AND SERIALNUMBER=11889004 AND METERTCTOTALCONSUMPT > 0 ORDER BY METERTCREADDT ASC";
 $rs = odbc_exec($connection, $query);
 
-if (!$rs){
+if (!$rs)
+{
     exit("Error in SQL");
 }
 
@@ -48,24 +51,59 @@ $mpn = 8;
 $divisor = 1000;
 $mpd = 10;
 
+/* Date Constants */
+$one_day = 24 * 60 * 60 * 1000;
+
 odbc_fetch_row($rs);
 $pulses = odbc_result($rs,"METERTCTOTALCONSUMPT");
 $prev_kwh = ($pulses * $kr * $kh * $mpn) / ($divisor * $mpd);
 $day_count = 1;
-while(odbc_fetch_row($rs)){
+
+while (odbc_fetch_row($rs))
+{
     $date = odbc_result($rs,"METERTCREADDT");
     $timestamp = strtotime($date) * 1000;
+
+    if (!isset($prev_timestamp))
+    {
+        $prev_timestamp = $timestamp;
+    }
+
+    $time_diff = ((($timestamp - $prev_timestamp) / 1000 ) / 60) / 60;
+
     $pulses = odbc_result($rs,"METERTCTOTALCONSUMPT");
     $current_kwh = ($pulses * $kr * $kh * $mpn) / ($divisor * $mpd);
     $daily_kwh = ($current_kwh - $prev_kwh) / $day_count;
     $daily_payoff = $daily_kwh * .055;
-    if ($prev_kwh > 0 and $current_kwh > 0){
-        $temp = array();
-        $temp[] = array('v' => "Date($timestamp)");
-        $temp[] = array('v' => (float) $daily_payoff);
-        $rows[] = array('c' => $temp);
+
+    if ($prev_kwh > 0 and $current_kwh > 0)
+    {
+        if ($time_diff < 25)
+        {
+            $temp = array();
+            $temp[] = array('v' => "Date($timestamp)");
+            $temp[] = array('v' => (float) $daily_payoff);
+            $rows[] = array('c' => $temp);
+        }
+        else 
+        {
+            $missing_days = $time_diff / 24;
+            $avg_daily_payoff = $daily_payoff / $missing_days;
+            $new_timestamp = $timestamp;
+
+            for ($i = 0; $i <= $missing_days; $i++) 
+            {
+                $temp = array();
+                $temp[] = array('v' => "Date($new_timestamp)");
+                $temp[] = array('v' => (float) ($avg_daily_payoff));
+                $rows[] = array('c' => $temp);
+                $new_timestamp = $new_timestamp - $one_day;
+            }
+        }
     }
+
     $prev_kwh = $current_kwh;
+    $prev_timestamp = $timestamp;
 }
 
 odbc_close($connection);
